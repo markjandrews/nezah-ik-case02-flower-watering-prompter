28282e4fe5dec9a12f5774504b72e48d7e0819ef
Mark <mark.andrews@internode.on.net>
2021-12-04 10:27:17 +1100

    Update pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,12 @@
+let i = 0
+basic.showIcon(IconNames.Happy)
 basic.forever(function () {
-	
+    i = PlanetX_Basic.soilHumidity(PlanetX_Basic.AnalogRJPin.J2)
+    if (i < 30) {
+        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, true)
+        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J4, false)
+    } else {
+        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J3, false)
+        PlanetX_Display.ledBrightness(PlanetX_Display.DigitalRJPin.J4, true)
+    }
 })
